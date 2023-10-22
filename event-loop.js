7520b6809f25db71a120fcdfdb8bd2878541a445
huanchin <cherry032826@gmail.com>
2023-10-23 03:36:38 +0800

    add thread pool example 3

--- event-loop.js
+++ event-loop.js
@@ -167,3 +167,55 @@ fs.readFile("test-file.txt", () => {
 });
 
 console.log("Hello from the top level code");
+
+
+/****** thread pool示範 3 ********/
+// 如果使用sync type encrypt則會block整個event-loop
+// 結果：
+// Hello from the top level code
+// Timer 1 finished
+// Immediate 1 finished
+// I/O finished
+// --------------
+// 551 Password encrypted --> 如果使用sync type encrypt則會block整個event-loop
+// 1091 Password encrypted --> 如果使用sync type encrypt則會block整個event-loop
+// 1629 Password encrypted --> 如果使用sync type encrypt則會block整個event-loop
+// 2166 Password encrypted --> 如果使用sync type encrypt則會block整個event-loop
+// Process.nextTick
+// Immediate 2 finished
+// Timer 2 finished
+// Timer 3 finished
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 1; // defualt thread pool size is 4
+
+setTimeout(() => console.log("Timer 1 finished"), 0);
+setImmediate(() => console.log("Immediate 1 finished"));
+
+fs.readFile("test-file.txt", () => {
+  console.log("I/O finished");
+  console.log("--------------");
+
+  setTimeout(() => console.log("Timer 2 finished"), 0);
+  setTimeout(() => console.log("Timer 3 finished"), 3000);
+  setImmediate(() => console.log("Immediate 2 finished"));
+
+  // When we pass a function to process.nextTick(), we instruct the engine to invoke this function at the end of the current operation, before the next event loop tick starts
+  process.nextTick(() => console.log("Process.nextTick"));
+
+  // belows these four password encryptions will no longer run in the event loop.
+  // And so they will no longer be offloaded to the thread pool.
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
+});
+
+console.log("Hello from the top level code");
